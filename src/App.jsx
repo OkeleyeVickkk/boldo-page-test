2eca4ee8f13a1f7c1acb89a5920c6cefb406a9ea
OkeleyeVickkk <justusofnigeria@gmail.com>
2025-04-26 10:33:18 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -26,22 +26,22 @@ function App() {
 
 	return (
 		<div className="bg-white relative flex flex-col">
-			<section className="min-h-[90vh] sm:min-h-screen lg:min-h-[95vh] overflow-hidden bg-primary">
+			<section className="min-h-[90vh] sm:min-h-screen md:min-h-[95vh] lg:min-h-[98vh] overflow-hidden bg-primary">
 				<Container mediaquery="xl:w-11/12 !px-0">
 					<Header />
-					<div className="pt-[100px] md:pt-5 lg:pt-10 mt-10 xl:pt-0">
-						<div className="flex text-center items-center flex-col gap-y-6 text-white lg:w-10/12 xl:w-7/12 mx-auto">
+					<div className="lg:w-11/12 mx-auto pt-[100px] md:pt-20 lg:pt-10 mt-10 xl:pt-0">
+						<div className="flex text-center items-center flex-col gap-y-6 text-white sm:w-10/12 lg:w-11/12 xl:w-7/12 mx-auto">
 							<div className="font-osans text-[17px] tracking-wider text-[#F1F1F1]">
 								<span>About</span>
 							</div>
 							<div className="font-manrope">
 								<FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-									<h2 className="text-4xl md:text-6xl xl:text-[64px] md:leading-[1.5] lg:leading-16 xl:leading-20 relative">
+									<h2 className="text-5xl md:text-[52px] lg:text-[52px] xl:text-6xl md:leading-[1.3] lg:leading-[1.3] xl:leading-20 relative">
 										We love to make great things, things that matter.
 									</h2>
 								</FadeContent>
 							</div>
-							<div className="font-osans w-11/12 mx-auto leading-8 mt-2 text-[#F1F1F1]">
+							<div className="font-osans w-11/12 mx-auto leading-8 text-[#F1F1F1]">
 								<p>
 									Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage innovator success deployment
 									non-disclosure.
